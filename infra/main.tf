37a3cfaec3004765003aa712dc3e1bed8c96c092
Djoums <eteil.djoumatchoua@gmail.com>
2025-02-13 14:45:14 +0100

    ajout temoignage ursule

--- infra/main.tf
+++ infra/main.tf
@@ -54,7 +54,7 @@ resource "aws_sns_topic" "sns_topic" {
 resource "aws_sns_topic_subscription" "email_subscription" {
   topic_arn = aws_sns_topic.sns_topic.arn
   protocol  = "email"
-  endpoint  = "eteilj@gmail.com" # Remplace par ton adresse e-mail
+  endpoint  = "Youndzofrancine@yahoo.fr" # Remplace par ton adresse e-mail
 }
 
 data "archive_file" "lambda_zip" {
